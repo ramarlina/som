3f82208a71ac8ced3629192f14250270efe9250b
ramarlina <m.ramarlina@gmail.com>
2013-01-19 18:22:59 -0600

    Remove unnecessary lines

--- som.py
+++ som.py
@@ -1,11 +1,6 @@
 import numpy
 import pandas
 from sklearn.decomposition import RandomizedPCA
-from matplotlib import pyplot as plt
-from matplotlib import cm
-from mpl_toolkits.mplot3d import axes3d
-from pylab import meshgrid
-plt.ion()
 
 class Cluster():
     def __init__(self, x, y):        
@@ -19,15 +14,17 @@ class Cluster():
         self.epoch = 0
         
     def train(self, X, iter, batch_size=1):
-        
         if len(self.map) == 0:
+            x,y = self.shape
             self.map = numpy.zeros((self.n_neurons, len(X[0])))
             self.labels = numpy.arange(len(X)).astype("S10")
-            eigen = RandomizedPCA(4).fit_transform(X.T).T
+            eigen = RandomizedPCA(10).fit_transform(X.T).T
             self.map[0] = eigen[0]
             self.map[y-1] = eigen[1]
             self.map[(x-1)*y] = eigen[2]
             self.map[x*y - 1] = eigen[3]
+            for i in range(4, 10):
+                self.map[numpy.random.randint(1, self.n_neurons)] = eigen[i]
         self.total = iter
         #alpha_decay == (0.1/a)**1/n
         self.alpha_decay = (self.alpha_final/self.alpha)**(1.0/self.total)
@@ -60,7 +57,7 @@ class Cluster():
         x, y = self.shape
         delta = self.map - vector
         dists = numpy.sum((delta)**2, axis=1).reshape(x,y)
-        idx = numpy.argmin(dists) 
+        idx = numpy.argmin(dists)
         print "Epoch ", self.epoch, ": ", (idx/x, idx%y), "; Sigma: ", self.sigma, "; alpha: ", self.alpha
         
         self.sigma = self.sigma*self.sigma_decay
@@ -87,21 +84,6 @@ class Cluster():
         self.shape = data[[0,1]]
         z = data[2]
         self.map = data[3:].reshape(self.shape[0]*self.shape[1], z)
-        
-        
-def make_copy(cl):
-    tmp = Cluster(numpy.random.normal(0,1,(5,5)), 2,2)
-    tmp.shape = cl.shape
-    tmp.labels = cl.labels
-    tmp.map =  cl.map
-    tmp.template = cl.template
-    tmp.alpha = cl.alpha
-    tmp.alpha_final = cl.alpha_final
-    tmp.epoch = cl.epoch
-    tmp.selected = cl.selected
-    tmp.data = cl.data
-    tmp.sigma = cl.sigma
-    return tmp
     
 from PIL import Image
 from tools import make_tile
