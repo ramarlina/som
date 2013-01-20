e5d3e5739abb9eed94e9c2462eb727bad667f67c
ramarlina <m.ramarlina@gmail.com>
2013-01-20 01:35:03 -0600

    Removed unecessary lines, added comments

--- som.py
+++ som.py
@@ -1,13 +1,12 @@
 import numpy
-import pandas
 from sklearn.decomposition import RandomizedPCA
 
-class Cluster():
+class SOM():
     def __init__(self, x, y):        
         self.map = []
         self.n_neurons = x*y
         self.sigma = x
-        self.template = numpy.arange(x*y)
+        self.template = numpy.arange(x*y).reshape(self.n_neurons,1)
         self.alpha = 0.6
         self.alpha_final = 0.1
         self.shape = [x,y]
@@ -16,18 +15,26 @@ class Cluster():
     def train(self, X, iter, batch_size=1):
         if len(self.map) == 0:
             x,y = self.shape
+            # first we initialize the map
             self.map = numpy.zeros((self.n_neurons, len(X[0])))
-            self.labels = numpy.arange(len(X)).astype("S10")
+            
+            # then we the pricipal components of the input data
             eigen = RandomizedPCA(10).fit_transform(X.T).T
+            
+            # then we set different point on the map equal to principal components to force diversification
             self.map[0] = eigen[0]
             self.map[y-1] = eigen[1]
             self.map[(x-1)*y] = eigen[2]
             self.map[x*y - 1] = eigen[3]
             for i in range(4, 10):
                 self.map[numpy.random.randint(1, self.n_neurons)] = eigen[i]
+                
         self.total = iter
-        #alpha_decay == (0.1/a)**1/n
+        
+        # coefficient of decay for learning rate alpha
         self.alpha_decay = (self.alpha_final/self.alpha)**(1.0/self.total)
+        
+        # coefficient of decay for gaussian smoothing
         self.sigma_decay = (numpy.sqrt(self.shape[0])/(4*self.sigma))**(1.0/self.total)
         
         samples = numpy.arange(len(X))
@@ -37,53 +44,49 @@ class Cluster():
             idx = samples[i:i + batch_size]
             self.iterate(X[idx])
     
-    def transform(self, X, plot = False):
-        plt.ion()
-        v = numpy.sum((self.map - X)**2, axis=1).reshape(self.shape[0], self.shape[1])
-        if plot:
-            plt.contourf(v, alpha=.7)
-        plt.draw()
-        return v.ravel()
-    
-    def predict(self, X):
-        #X = X.T
-        x = self.shape[0]
-        y = self.shape[1]
-        dists = numpy.sum((self.map - X)**2, axis=1)
-        idx = numpy.argmin(dists)
-        return [idx%y, idx/x]
+    def transform(self, X):
+        # We simply compute the dot product of the input with the transpose of the map to get the new input vectors
+        res = numpy.dot(X,self.map.T)
+        
+        # We scale down new input vectors to values between 0 and 1
+        res = res / (numpy.sum(self.map, axis=1))
+        return res
      
     def iterate(self, vector):  
         x, y = self.shape
+        
         delta = self.map - vector
+        
+        # Euclidian distance of each neurons with the example
         dists = numpy.sum((delta)**2, axis=1).reshape(x,y)
+        
+        # Best maching unit
         idx = numpy.argmin(dists)
         print "Epoch ", self.epoch, ": ", (idx/x, idx%y), "; Sigma: ", self.sigma, "; alpha: ", self.alpha
         
+        # Linearly reducing the width of Gaussian Kernel
         self.sigma = self.sigma*self.sigma_decay
-        dist_map = self.template.reshape(x,y)        
-        dists = numpy.sqrt((dist_map/x - idx/x)**2 + (numpy.mod(dist_map,x) - idx%y)**2)
+        dist_map = self.template.reshape(x,y)     
         
+        # Distance of each neurons in the map from the best matching neuron
+        dists = numpy.sqrt((dist_map/x - idx/x)**2 + (numpy.mod(dist_map,x) - idx%y)**2).reshape(self.n_neurons, 1)
+        #dists = self.template - idx
+        
+        # Applying Gaussian smoothing to distances of neurons from best matching neuron
         h = numpy.exp(-(dists/self.sigma)**2)      
          
-        for cell in xrange(len(self.map)):
-            self.map[cell] = self.map[cell] - self.alpha*h[cell%x, cell/y]*delta[cell]
+        # Updating neurons in the map
+        self.map -= self.alpha*h*delta
        
-        
-        #0.1 = a*d**n --> d**n = 0.1/a --> d == (0.1/a)**1/n
+        # Decreasing alpha
         self.alpha = self.alpha*self.alpha_decay
         
         self.epoch = self.epoch + 1 
         
-    def save(self, filename="som_weights.csv"):
-        data = numpy.concatenate([self.shape, [len(self.data[0])], self.map.ravel()])
-        pandas.DataFrame(data).to_csv(filename)
         
-    def load(self):
-        data = pandas.read_csv("som_weights.csv").values[:,1].ravel()
-        self.shape = data[[0,1]]
-        z = data[2]
-        self.map = data[3:].reshape(self.shape[0]*self.shape[1], z)
+##################################################### 
+#       EXAMPLE: TRAINING SOM ON MNIST DATA         #
+#####################################################       
     
 from PIL import Image
 from tools import make_tile
@@ -94,17 +97,25 @@ def load_mnist():
     f = gzip.open("mnist/mnist.pkl.gz", 'rb')
     train, valid, test = cPickle.load(f)
     f.close()  
-    return train[0]
+    return train[0][:20000],train[1][:20000]
     
 def demo():
     # Get data
-    X = load_mnist()
-    cl = Cluster(20, 20)
+    X, y = load_mnist()
+    cl = SOM(20, 20)
     cl.train(X, 2000)  
+    
+    # Plotting hidden units
     W = cl.map
     W = make_tile(W, img_shape= (28,28), tile_shape=(20,20))
     img = Image.fromarray(W)
-    img.save("som_batch_size_5.png")
+    img.save("som_results.png")
+    
+    # creating new inputs
+    #newX = cl.transform(X)
+    
+    return cl.map, X, y
+    
     
 if __name__ == '__main__':
     demo()
